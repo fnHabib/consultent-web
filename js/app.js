8b112aa8e7ad0bd5158998d175bc4f5b54b1adc8
habibur <habibur7666@gmail.com>
2021-10-04 21:10:18 +0600

    today i complete happy client section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
-let ul = document.querySelector('.nav');
+let myul = document.getElementById('newul');
+let li = document.querySelectorAll('.foreachme');
+let mainimg = document.querySelector('.mainimg');
+let sidediv1 =document.querySelectorAll('.relationProblem');
+myul.addEventListener('click',myfunc);
 
-ul.parentElement.addEventListener("click", target)
+function myfunc(e){
+
+            
+        li.forEach(li => {li.classList.remove('active')});
+        e.target.classList.add('active');
+
+        if(e.target.innHTML === li.innerHTML){
+            
+            sidediv1.classList.add('d-none')
+            sidediv2.classList.remove('d-none');
+            sidediv2.classList.add('d-block');
+
+           }
+            
+            
+            
+        
+      
+}
+
+
+
+
+
+
